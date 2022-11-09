aa187c51fa98930600dff85f5bdb3338a99201ac
karan-IWebCode <karan.vikas@iwebcode.net>
2022-11-09 18:15:18 +0530

    react forms

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import './Header.css'
+import headImg  from '../../Assets/headerImage.png'
+
+const Header = () => {
+    return(
+        <>
+            <div className="head">
+                <img src={headImg} className="img"/>
+            </div>
+            <div className="heading">
+                <h2 className="">Full-Stack Engineer</h2>
+                <p className="">REMOTE OPTIONAL /PRODUCT – ENGINEERING /FULL-TIME</p>
+            </div>
+        </>
+    )
+}
+
+export default Header; 
